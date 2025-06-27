9e805602f741568652817a9e86f0b3da9cf6e4af
Fransuelton <elton6103@gmail.com>
2025-06-27 00:09:08 -0300

    ✨ feat: enable CORS support in the application

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import ideaRoutes from "./routes/ideaRoutes.js";
+import cors from "cors"
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(ideaRoutes);
 
